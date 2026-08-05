4eb789ba5cddc50a5bfd52e50b77e0dfe9ed0646
hemasai-axiora <hemasai.s@axioraglobalsolutions.com>
2026-08-05 12:30:42 +0530

    Wrap entire App.php __construct in try-catch to print constructor errors

--- app/core/App.php
+++ app/core/App.php
@@ -7,53 +7,53 @@ class App {
     protected $params = [];
 
     public function __construct() {
-        $route = $this->getRoute();
-
-        // Parse route: controller/method
-        if ($route) {
-            $parts = explode('/', trim($route, '/'));
-            
-            // Format controller name (e.g. auth -> AuthController)
-            if (isset($parts[0])) {
-                $rawName = $parts[0];
-                $studlyName = str_replace(' ', '', ucwords(str_replace(['_', '-'], ' ', $rawName)));
-                $controllerName = $studlyName . 'Controller';
-                if (!file_exists(APPROOT . '/controllers/' . $controllerName . '.php')) {
-                    $controllerName = ucfirst($rawName) . 'Controller';
-                }
-                if (file_exists(APPROOT . '/controllers/' . $controllerName . '.php')) {
-                    $this->currentController = $controllerName;
-                    unset($parts[0]);
-                }
-            }
-
-            // Require the controller
-            require_once APPROOT . '/controllers/' . $this->currentController . '.php';
-            $this->currentController = new $this->currentController;
-
-            // Check for method name
-            if (isset($parts[1])) {
-                $methodName = $parts[1];
-                $camelMethod = lcfirst(str_replace(' ', '', ucwords(str_replace('-', ' ', $methodName))));
-                if ($methodName !== '' && $methodName[0] !== '_' && is_callable([$this->currentController, $methodName])) {
-                    $this->currentMethod = $methodName;
-                    unset($parts[1]);
-                } elseif ($camelMethod !== '' && is_callable([$this->currentController, $camelMethod])) {
-                    $this->currentMethod = $camelMethod;
-                    unset($parts[1]);
-                }
-            }
-
-            // Get parameters
-            $this->params = $parts ? array_values($parts) : [];
-        } else {
-            // Default controller
-            require_once APPROOT . '/controllers/' . $this->currentController . '.php';
-            $this->currentController = new $this->currentController;
-        }
-
-        // Call a callback with array of params
         try {
+            $route = $this->getRoute();
+
+            // Parse route: controller/method
+            if ($route) {
+                $parts = explode('/', trim($route, '/'));
+                
+                // Format controller name (e.g. auth -> AuthController)
+                if (isset($parts[0])) {
+                    $rawName = $parts[0];
+                    $studlyName = str_replace(' ', '', ucwords(str_replace(['_', '-'], ' ', $rawName)));
+                    $controllerName = $studlyName . 'Controller';
+                    if (!file_exists(APPROOT . '/controllers/' . $controllerName . '.php')) {
+                        $controllerName = ucfirst($rawName) . 'Controller';
+                    }
+                    if (file_exists(APPROOT . '/controllers/' . $controllerName . '.php')) {
+                        $this->currentController = $controllerName;
+                        unset($parts[0]);
+                    }
+                }
+
+                // Require the controller
+                require_once APPROOT . '/controllers/' . $this->currentController . '.php';
+                $this->currentController = new $this->currentController;
+
+                // Check for method name
+                if (isset($parts[1])) {
+                    $methodName = $parts[1];
+                    $camelMethod = lcfirst(str_replace(' ', '', ucwords(str_replace('-', ' ', $methodName))));
+                    if ($methodName !== '' && $methodName[0] !== '_' && is_callable([$this->currentController, $methodName])) {
+                        $this->currentMethod = $methodName;
+                        unset($parts[1]);
+                    } elseif ($camelMethod !== '' && is_callable([$this->currentController, $camelMethod])) {
+                        $this->currentMethod = $camelMethod;
+                        unset($parts[1]);
+                    }
+                }
+
+                // Get parameters
+                $this->params = $parts ? array_values($parts) : [];
+            } else {
+                // Default controller
+                require_once APPROOT . '/controllers/' . $this->currentController . '.php';
+                $this->currentController = new $this->currentController;
+            }
+
+            // Call a callback with array of params
             call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
         } catch (Throwable $e) {
             http_response_code(200);
